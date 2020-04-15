7c684f618bc35083be7b86bab05886bc0f433b2d
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2020-04-15 10:15:04 +0200

    Implement knockback

--- src/Player.gd
+++ src/Player.gd
@@ -7,59 +7,108 @@ export var DAMPING_DIVIDER = 0.2
 export var MIN_VELOCITY = 10
 export var AIR_DAMPING_MULTIPLIER = 2
 export var AIR_ACCELERATION_MULTIPLIER = 0.8
+export var KNOCKBACK_POWER = 100
+export var KNOCKBACK_TIME = 1.0
+export var ODIN_TOUCH_DAMAGE = 1.0
 
 export var VELOCITY_ANIMATION_THRESHOLD = 0.1
+
+enum states {
+    move,
+    sword_attack,
+    bow_attack,
+    knockback,
+    bow_prepare,
+    intro,
+}
 
 onready var sprite = get_node("Sprite")
 onready var anim = get_node("Sprite/AnimationPlayer")
 onready var hitbox = get_node("Hitbox")
+onready var health_bar = get_node("Sprite/Lock/HealthBar")
+onready var arrow_bar = get_node("Sprite/Lock/ArrowBar")
 
 var velocity = Vector2(0,0)
+var state = states.move
+var enable_movement = true
+var health = 0.0
 
 func _ready():
     hitbox.connect("body_entered",self,"on_hitbox_entered")
+    health = health_bar.MAX_VALUE
 
 func ground_movement(delta):
     var acceleration = ACCELERATION*delta
     if not is_on_floor():
         acceleration *= AIR_ACCELERATION_MULTIPLIER
-    if Input.is_action_pressed("move_right"):
+    if enable_movement and Input.is_action_pressed("move_right"):
         velocity.x = min(velocity.x + acceleration,VELOCITY)
-    elif Input.is_action_pressed("move_left"):
+    elif enable_movement and Input.is_action_pressed("move_left"):
         velocity.x = max(velocity.x - acceleration,-VELOCITY)
     else:
-        var damping = -sign(velocity.x) * VELOCITY/(DAMPING_DIVIDER)*delta
-        if not is_on_floor():
-            damping /= AIR_DAMPING_MULTIPLIER
-        velocity.x += damping
-        if abs(velocity.x) < MIN_VELOCITY:
-            velocity.x = 0
+        if state != states.knockback or is_on_floor():
+            var damping = -sign(velocity.x) * VELOCITY/(DAMPING_DIVIDER)*delta
+            if not is_on_floor():
+                damping /= AIR_DAMPING_MULTIPLIER
+            velocity.x += damping
+            if abs(velocity.x) < MIN_VELOCITY:
+                velocity.x = 0
 
     if is_on_floor():
-        if Input.is_action_just_pressed("jump"):
+        if enable_movement and Input.is_action_just_pressed("jump"):
             velocity.y -= JUMP_POWER
     velocity += Globals.GRAVITY * delta
+
+var time_val = 0.0
+
+func handle_states(delta):
+    match state:
+        states.move:
+            if abs(velocity.x) > VELOCITY_ANIMATION_THRESHOLD:
+                if velocity.x < 0:
+                    sprite.flip_h = true
+                else:
+                    sprite.flip_h = false
+            if is_on_floor():
+                if abs(velocity.x) > VELOCITY_ANIMATION_THRESHOLD:
+                    anim.play("Run")
+                else:
+                    anim.play("Stand")
+            else:
+                if velocity.y < 0:
+                    anim.play("JumpUp")
+                else:
+                    anim.play("JumpDown")
+        states.knockback:
+            time_val += delta
+            if time_val > KNOCKBACK_TIME:
+                enable_movement = true
+                state = states.move
 
 func _physics_process(delta):
     ground_movement(delta)
     velocity = move_and_slide(velocity,Vector2(0,-1))
 
-func _process(_delta):
-    if abs(velocity.x) > VELOCITY_ANIMATION_THRESHOLD:
-        if velocity.x < 0:
-            sprite.flip_h = true
+func _process(delta):
+    handle_states(delta)
+    
+func damage(amount):
+    health = max(health - amount,0.0)
+    health_bar.set_value(health)
+    
+func knockback(pos):
+    if state != states.knockback:
+        if Globals.state == Globals.states.boss_ground:
+            anim.play("KnockbackGround")
         else:
-            sprite.flip_h = false
-    if is_on_floor():
-        if abs(velocity.x) > VELOCITY_ANIMATION_THRESHOLD:
-            anim.play("Run")
-        else:
-            anim.play("Stand")
-    else:
-        if velocity.y < 0:
-            anim.play("JumpUp")
-        else:
-            anim.play("JumpDown")
+            anim.play("KnockbackFall")
+        velocity = (position - pos).normalized() * KNOCKBACK_POWER
+        enable_movement = false
+        time_val = 0.0
+    state = states.knockback
 
 func on_hitbox_entered(body):
     print(body)
+    if body.collision_layer & 2:
+        knockback(body.position + Vector2(0,60))
+        damage(ODIN_TOUCH_DAMAGE)
--- src/globals.gd
+++ src/globals.gd
@@ -1,5 +1,22 @@
 extends Node
 
+enum states {
+    menu,
+    boss_ground,
+    boss_fall,
+    boss_destroy_ground,
+    ground_fall_transition,
+    ground_intro,
+    game_over,
+    intro,
+    ground_win,
+    fall_win_transition,
+    ground_win_intro,
+    win,
+}
+
 var GAME_WIDTH = 320
 var GAME_HEIGHT = 180
 var GRAVITY = Vector2(0,125)
+
+var state = states.boss_ground

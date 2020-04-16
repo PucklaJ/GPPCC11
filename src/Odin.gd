b1672ca96503eb4c41ea707f12ac57634c16eb9d
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2020-04-16 11:31:04 +0200

    Add axe of Odin

--- src/Odin.gd
+++ src/Odin.gd
@@ -30,10 +30,15 @@ export var KNOCKBACK_TIME = 2.0
 export var KNOCKBACK_EXPLOSION_WAIT_TIME = 0.25
 export var KNOCKBACK_EXPLOSION_TIME = 0.5
 export var KNOCKBACK_EXPLOSION_RADIUS = 100.0
+export var HIT_ON_GROUND_RADIUS = 100.0
+export var WAIT_FOR_HIT_ON_GROUND_TIME = 1.0
+export var HIT_ON_GROUND_TIME = 0.5
+export var AXE_DAMAGE = 2.0
 
 onready var sprite = get_node("Sprite")
 onready var anim = sprite.get_node("AnimationPlayer")
 onready var health_bar = sprite.get_node("Lock/HealthBar")
+onready var axe_hitbox = sprite.get_node("AxeHitbox")
 
 var width = 50
 var height = 75
@@ -45,6 +50,7 @@ var player = null
 
 func _ready():
     health = health_bar.MAX_VALUE
+    axe_hitbox.connect("body_entered",self,"on_axe_entered")
 
 func ground_movement(delta):
     velocity += Globals.GRAVITY * delta
@@ -53,6 +59,8 @@ func ground_movement(delta):
 var time_val = 0.0
 
 func handle_states(delta):
+    sprite.position = Vector2(0,0)
+    axe_hitbox.position = Vector2(0,0)
     match state:
         states.walk:
             anim.play("Run")
@@ -68,6 +76,15 @@ func handle_states(delta):
                     position.x = Globals.GAME_WIDTH - GROUND_WALK_POINT - width / 2.0
                     state = states.wait_for_walk
                     time_val = 0.0
+            if player.position.distance_squared_to(position) < HIT_ON_GROUND_RADIUS * HIT_ON_GROUND_RADIUS:
+                state = states.wait_for_hit_on_ground
+                time_val = 0.0
+                anim.play("HitWait1")
+                velocity.x = 0.0
+                if player.position.x > position.x:
+                    sprite.flip_h = true
+                else:
+                    sprite.flip_h = false
         states.wait_for_walk:
             anim.play("Stand")
             velocity.x = 0.0
@@ -100,6 +117,29 @@ func handle_states(delta):
                     anim.play("Fall")
                 state = states.walk
                 time_val = 0.0
+        states.wait_for_hit_on_ground:
+            if not sprite.flip_h:
+                sprite.position = Vector2(-50,0)
+            else:
+                axe_hitbox.position = Vector2(39,0)
+            time_val += delta
+            if time_val > WAIT_FOR_HIT_ON_GROUND_TIME:
+                anim.play("Hit")
+                time_val = 0.0
+                state = states.hit_on_ground
+        states.hit_on_ground:
+            if not sprite.flip_h:
+                sprite.position = Vector2(-50,0)
+            else:
+                axe_hitbox.position = Vector2(39,0)
+            time_val += delta
+            if time_val > HIT_ON_GROUND_TIME:
+                time_val = 0.0
+                state = states.walk
+                if Globals.state == Globals.states.boss_ground:
+                    anim.play("Stand")
+                else:
+                    anim.play("Fall")
 
 func _physics_process(delta):
     ground_movement(delta)
@@ -112,6 +152,7 @@ func knockback():
     velocity.x = 0.0
     anim.play("Knockback")
     time_val = 0.0
+    axe_hitbox.monitoring = false
 
 func damage(amount):
     health = max(health-amount,0.0)
@@ -128,3 +169,8 @@ func can_be_damaged():
 
 func can_inflict_damage():
     return can_be_damaged()
+
+func on_axe_entered(body):
+    if body.can_be_damaged():
+        body.knockback(axe_hitbox.get_child(0).global_position)
+        body.damage(AXE_DAMAGE)

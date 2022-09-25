85e0f2574923502be5ffd8cf986c227208048b73
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-09-25 18:24:53 +0200

    Replace border LineSphapes with RectangleShapes

--- src/Odin.gd
+++ src/Odin.gd
@@ -1,26 +1,27 @@
 extends KinematicBody2D
 
 enum states{
-    walk,
-    wait_for_walk,
-    wait_for_second_speer,
-    wait_for_hit_on_ground,
-    hit_on_ground,
-    wait_for_walk_hit_on_ground,
-    knockback,
-    awake_from_knockback,
-    knockback_explosion,
-    destroy_ground_jump_up,
-    destroy_ground_jump_down,
-    destroy_ground,
-    teleport_wait,
-    teleport_attack,
-    intro,
-    fall_game_over,
-    ground_game_over,
-    wait_for_walk_game_over,
-    wait_for_grab_new_speer,
-    wait_for_lightning_throw
+	walk,
+	wait_for_walk,
+	wait_for_second_speer,
+	wait_for_hit_on_ground,
+	hit_on_ground,
+	wait_for_walk_hit_on_ground,
+	knockback,
+	awake_from_knockback,
+	knockback_explosion,
+	destroy_ground_jump_up,
+	destroy_ground_jump_down,
+	destroy_ground,
+	teleport_wait,
+	teleport_attack,
+	intro,
+	fall_game_over,
+	ground_game_over,
+	wait_for_walk_game_over,
+	wait_for_grab_new_speer,
+	wait_for_lightning_throw,
+	none
 }
 
 export var GROUND_VELOCITY = 50
@@ -56,192 +57,192 @@ var player = null
 var speer_thrown = 0
 
 func _ready():
-    health = health_bar.MAX_VALUE
-    axe_hitbox.connect("body_entered",self,"on_axe_entered")
+	health = health_bar.MAX_VALUE
+	axe_hitbox.connect("body_entered",self,"on_axe_entered")
 
 func ground_movement(delta):
-    velocity += Globals.GRAVITY * delta
-    velocity = move_and_slide(velocity,Vector2(0,-1))
+	velocity += Globals.GRAVITY * delta
+	velocity = move_and_slide(velocity,Vector2(0,-1))
 
 var time_val = 0.0
 
 func handle_states(delta):
-    sprite.position = Vector2(0,0)
-    axe_hitbox.position = Vector2(0,0)
-    speer_start.position = Vector2(15,34)
-    match state:
-        states.walk:
-            anim.play("Run")
-            if not sprite.flip_h:
-                velocity.x = -GROUND_VELOCITY
-                if position.x + width / 2.0 < GROUND_WALK_POINT:
-                    position.x = GROUND_WALK_POINT - width / 2.0
-                    state = states.wait_for_walk
-                    time_val = 0.0
-            else:
-                velocity.x = GROUND_VELOCITY
-                if position.x + width / 2.0 > Globals.GAME_WIDTH - GROUND_WALK_POINT:
-                    position.x = Globals.GAME_WIDTH - GROUND_WALK_POINT - width / 2.0
-                    state = states.wait_for_walk
-                    time_val = 0.0
-            if player.get_position().distance_squared_to(get_position()) < HIT_ON_GROUND_RADIUS * HIT_ON_GROUND_RADIUS:
-                state = states.wait_for_hit_on_ground
-                time_val = 0.0
-                anim.play("HitWait1")
-                velocity.x = 0.0
-                if player.position.x > position.x:
-                    sprite.flip_h = true
-                else:
-                    sprite.flip_h = false
-            elif time_val > SPEER_THROW_FREQUENCY:
-                time_val = 0.0
-                if Globals.state == Globals.states.boss_ground:
-                    anim.play("SpeerWaitGround")
-                else:
-                    anim.play("SpeerWaitFall")
-                velocity.x = 0.0
-                if player.get_position().x > get_position().x:
-                    sprite.flip_h = true
-                else:
-                    sprite.flip_h = false
-                state = states.wait_for_second_speer
-            time_val += delta
-        states.wait_for_walk:
-            anim.play("Stand")
-            velocity.x = 0.0
-            if time_val > WAIT_FOR_WALK_TIME:
-                sprite.flip_h = not sprite.flip_h
-                state = states.walk
-            time_val += delta
-            if player.get_position().distance_squared_to(get_position()) < HIT_ON_GROUND_RADIUS * HIT_ON_GROUND_RADIUS:
-                state = states.wait_for_hit_on_ground
-                time_val = 0.0
-                anim.play("HitWait1")
-                velocity.x = 0.0
-                if player.position.x > position.x:
-                    sprite.flip_h = true
-                else:
-                    sprite.flip_h = false
-        states.knockback:
-            time_val += delta
-            if time_val > KNOCKBACK_TIME:
-                if Globals.state == Globals.states.boss_ground:
-                    anim.play("Stand")
-                else:
-                    anim.play("Fall")
-                state = states.awake_from_knockback
-                time_val = 0.0
-        states.awake_from_knockback:
-            time_val += delta
-            if time_val > KNOCKBACK_EXPLOSION_WAIT_TIME:
-                anim.play("KnockbackExplosion")
-                state = states.knockback_explosion
-                time_val = 0.0
-                knockback_explosion()
-        states.knockback_explosion:
-            time_val += delta
-            if time_val > KNOCKBACK_EXPLOSION_TIME:
-                if Globals.state == Globals.states.boss_ground:
-                    anim.play("Stand")
-                else:
-                    anim.play("Fall")
-                state = states.walk
-                time_val = 0.0
-        states.wait_for_hit_on_ground:
-            if not sprite.flip_h:
-                sprite.position = Vector2(-50,0)
-            else:
-                axe_hitbox.position = Vector2(39,0)
-            time_val += delta
-            if time_val > WAIT_FOR_HIT_ON_GROUND_TIME:
-                anim.play("Hit")
-                time_val = 0.0
-                state = states.hit_on_ground
-        states.hit_on_ground:
-            if not sprite.flip_h:
-                sprite.position = Vector2(-50,0)
-            else:
-                axe_hitbox.position = Vector2(39,0)
-            time_val += delta
-            if time_val > HIT_ON_GROUND_TIME:
-                time_val = 0.0
-                state = states.walk
-                if Globals.state == Globals.states.boss_ground:
-                    anim.play("Stand")
-                else:
-                    anim.play("Fall")
-        states.wait_for_second_speer:
-            time_val += delta
-            if sprite.flip_h:
-                sprite.position = Vector2(-20,0)
-                speer_start.position = Vector2(55,34)
-            if time_val > SPEER_WAIT_TIME:
-                if speer_thrown == SPEER_THROW_AMOUNT:
-                    state = states.walk
-                    if Globals.state == Globals.states.boss_ground:
-                        anim.play("Stand")
-                    else:
-                        anim.play("Fall")
-                    speer_thrown = 0
-                    time_val = 0.0
-                else:
-                    if Globals.state == Globals.states.boss_ground:
-                        anim.play("SpeerThrowGround")
-                    else:
-                        anim.play("SpeerThrowFall")
-                    time_val = 0.0
-                    throw_speer()
-                    state = states.wait_for_grab_new_speer
-        states.wait_for_grab_new_speer:
-            time_val += delta
-            if sprite.flip_h:
-                sprite.position = Vector2(-20,0)
-                speer_start.position = Vector2(55,34)
-            if time_val > SPEER_THROW_TIME:
-                time_val = 0.0
-                state = states.wait_for_second_speer
+	sprite.position = Vector2(0,0)
+	axe_hitbox.position = Vector2(0,0)
+	speer_start.position = Vector2(15,34)
+	match state:
+		states.walk:
+			anim.play("Run")
+			if not sprite.flip_h:
+				velocity.x = -GROUND_VELOCITY
+				if position.x + width / 2.0 < GROUND_WALK_POINT:
+					position.x = GROUND_WALK_POINT - width / 2.0
+					state = states.wait_for_walk
+					time_val = 0.0
+			else:
+				velocity.x = GROUND_VELOCITY
+				if position.x + width / 2.0 > Globals.GAME_WIDTH - GROUND_WALK_POINT:
+					position.x = Globals.GAME_WIDTH - GROUND_WALK_POINT - width / 2.0
+					state = states.wait_for_walk
+					time_val = 0.0
+			if player.get_position().distance_squared_to(get_position()) < HIT_ON_GROUND_RADIUS * HIT_ON_GROUND_RADIUS:
+				state = states.wait_for_hit_on_ground
+				time_val = 0.0
+				anim.play("HitWait1")
+				velocity.x = 0.0
+				if player.position.x > position.x:
+					sprite.flip_h = true
+				else:
+					sprite.flip_h = false
+			elif time_val > SPEER_THROW_FREQUENCY:
+				time_val = 0.0
+				if Globals.state == Globals.states.boss_ground:
+					anim.play("SpeerWaitGround")
+				else:
+					anim.play("SpeerWaitFall")
+				velocity.x = 0.0
+				if player.get_position().x > get_position().x:
+					sprite.flip_h = true
+				else:
+					sprite.flip_h = false
+				state = states.wait_for_second_speer
+			time_val += delta
+		states.wait_for_walk:
+			anim.play("Stand")
+			velocity.x = 0.0
+			if time_val > WAIT_FOR_WALK_TIME:
+				sprite.flip_h = not sprite.flip_h
+				state = states.walk
+			time_val += delta
+			if player.get_position().distance_squared_to(get_position()) < HIT_ON_GROUND_RADIUS * HIT_ON_GROUND_RADIUS:
+				state = states.wait_for_hit_on_ground
+				time_val = 0.0
+				anim.play("HitWait1")
+				velocity.x = 0.0
+				if player.position.x > position.x:
+					sprite.flip_h = true
+				else:
+					sprite.flip_h = false
+		states.knockback:
+			time_val += delta
+			if time_val > KNOCKBACK_TIME:
+				if Globals.state == Globals.states.boss_ground:
+					anim.play("Stand")
+				else:
+					anim.play("Fall")
+				state = states.awake_from_knockback
+				time_val = 0.0
+		states.awake_from_knockback:
+			time_val += delta
+			if time_val > KNOCKBACK_EXPLOSION_WAIT_TIME:
+				anim.play("KnockbackExplosion")
+				state = states.knockback_explosion
+				time_val = 0.0
+				knockback_explosion()
+		states.knockback_explosion:
+			time_val += delta
+			if time_val > KNOCKBACK_EXPLOSION_TIME:
+				if Globals.state == Globals.states.boss_ground:
+					anim.play("Stand")
+				else:
+					anim.play("Fall")
+				state = states.walk
+				time_val = 0.0
+		states.wait_for_hit_on_ground:
+			if not sprite.flip_h:
+				sprite.position = Vector2(-50,0)
+			else:
+				axe_hitbox.position = Vector2(39,0)
+			time_val += delta
+			if time_val > WAIT_FOR_HIT_ON_GROUND_TIME:
+				anim.play("Hit")
+				time_val = 0.0
+				state = states.hit_on_ground
+		states.hit_on_ground:
+			if not sprite.flip_h:
+				sprite.position = Vector2(-50,0)
+			else:
+				axe_hitbox.position = Vector2(39,0)
+			time_val += delta
+			if time_val > HIT_ON_GROUND_TIME:
+				time_val = 0.0
+				state = states.walk
+				if Globals.state == Globals.states.boss_ground:
+					anim.play("Stand")
+				else:
+					anim.play("Fall")
+		states.wait_for_second_speer:
+			time_val += delta
+			if sprite.flip_h:
+				sprite.position = Vector2(-20,0)
+				speer_start.position = Vector2(55,34)
+			if time_val > SPEER_WAIT_TIME:
+				if speer_thrown == SPEER_THROW_AMOUNT:
+					state = states.walk
+					if Globals.state == Globals.states.boss_ground:
+						anim.play("Stand")
+					else:
+						anim.play("Fall")
+					speer_thrown = 0
+					time_val = 0.0
+				else:
+					if Globals.state == Globals.states.boss_ground:
+						anim.play("SpeerThrowGround")
+					else:
+						anim.play("SpeerThrowFall")
+					time_val = 0.0
+					throw_speer()
+					state = states.wait_for_grab_new_speer
+		states.wait_for_grab_new_speer:
+			time_val += delta
+			if sprite.flip_h:
+				sprite.position = Vector2(-20,0)
+				speer_start.position = Vector2(55,34)
+			if time_val > SPEER_THROW_TIME:
+				time_val = 0.0
+				state = states.wait_for_second_speer
 
 func _physics_process(delta):
-    ground_movement(delta)
+	ground_movement(delta)
 
 func _process(delta):
-    handle_states(delta)
+	handle_states(delta)
 
 func knockback():
-    state = states.knockback
-    velocity.x = 0.0
-    anim.play("Knockback")
-    time_val = 0.0
-    speer_thrown = 0.0
-    axe_hitbox.monitoring = false
+	state = states.knockback
+	velocity.x = 0.0
+	anim.play("Knockback")
+	time_val = 0.0
+	speer_thrown = 0.0
+	axe_hitbox.monitoring = false
 
 func damage(amount):
-    health = max(health-amount,0.0)
-    health_bar.set_value(health)
+	health = max(health-amount,0.0)
+	health_bar.set_value(health)
 
 func knockback_explosion():
-    if player.get_position().distance_squared_to(get_position()) < KNOCKBACK_EXPLOSION_RADIUS*KNOCKBACK_EXPLOSION_RADIUS:
-        player.knockback(position+Vector2(0,60),2.0)
+	if player.get_position().distance_squared_to(get_position()) < KNOCKBACK_EXPLOSION_RADIUS*KNOCKBACK_EXPLOSION_RADIUS:
+		player.knockback(position+Vector2(0,60),2.0)
 
 func can_be_damaged():
-    match state:
-        states.knockback,states.awake_from_knockback,states.knockback_explosion: return false
-    return true
+	match state:
+		states.knockback,states.awake_from_knockback,states.knockback_explosion: return false
+	return true
 
 func can_inflict_damage():
-    return can_be_damaged()
+	return can_be_damaged()
 
 func on_axe_entered(body):
-    if body.can_be_damaged():
-        body.knockback(axe_hitbox.get_child(0).global_position)
-        body.damage(AXE_DAMAGE)
+	if body.can_be_damaged():
+		body.knockback(axe_hitbox.get_child(0).global_position)
+		body.damage(AXE_DAMAGE)
 
 func get_position():
-    return position + Vector2(width,height)/2.0
+	return position + Vector2(width,height)/2.0
 
 func throw_speer():
-    speer_thrown += 1
-    var speer = speer_scene.instance()
-    speer.position = speer_start.global_position
-    speer.direction = (player.get_position() - speer_start.global_position).normalized()
-    get_tree().get_root().add_child(speer)
+	speer_thrown += 1
+	var speer = speer_scene.instance()
+	speer.position = speer_start.global_position
+	speer.direction = (player.get_position() - speer_start.global_position).normalized()
+	get_tree().get_root().add_child(speer)
--- src/Player.gd
+++ src/Player.gd
@@ -19,12 +19,12 @@ export var START_ARROW_AMOUNT = 0.0
 export var VELOCITY_ANIMATION_THRESHOLD = 0.1
 
 enum states {
-    move,
-    sword_attack,
-    bow_attack,
-    knockback,
-    bow_prepare,
-    intro,
+	move,
+	sword_attack,
+	bow_attack,
+	knockback,
+	bow_prepare,
+	intro,
 }
 
 onready var arrow_scene = preload("res://objects/Arrow.tscn")
@@ -47,164 +47,164 @@ var width = 20
 var height = 25
 
 func _ready():
-    hitbox.connect("body_entered",self,"on_hitbox_entered")
-    sword_hitbox.connect("body_entered",self,"on_sword_hitbox_entered")
-    health = health_bar.MAX_VALUE
-    arrows = START_ARROW_AMOUNT
-    arrow_bar.set_value(arrows)
+	hitbox.connect("body_entered",self,"on_hitbox_entered")
+	sword_hitbox.connect("body_entered",self,"on_sword_hitbox_entered")
+	health = health_bar.MAX_VALUE
+	arrows = START_ARROW_AMOUNT
+	arrow_bar.set_value(arrows)
 
 func ground_movement(delta):
-    var acceleration = ACCELERATION*delta
-    if not is_on_floor():
-        acceleration *= AIR_ACCELERATION_MULTIPLIER
-    if enable_movement and Input.is_action_pressed("move_right"):
-        velocity.x = min(velocity.x + acceleration,VELOCITY)
-    elif enable_movement and Input.is_action_pressed("move_left"):
-        velocity.x = max(velocity.x - acceleration,-VELOCITY)
-    else:
-        if state != states.knockback or is_on_floor():
-            var damping = -sign(velocity.x) * VELOCITY/(DAMPING_DIVIDER)*delta
-            if not is_on_floor():
-                damping /= AIR_DAMPING_MULTIPLIER
-            velocity.x += damping
-            if abs(velocity.x) < MIN_VELOCITY:
-                velocity.x = 0
+	var acceleration = ACCELERATION*delta
+	if not is_on_floor():
+		acceleration *= AIR_ACCELERATION_MULTIPLIER
+	if enable_movement and Input.is_action_pressed("move_right"):
+		velocity.x = min(velocity.x + acceleration,VELOCITY)
+	elif enable_movement and Input.is_action_pressed("move_left"):
+		velocity.x = max(velocity.x - acceleration,-VELOCITY)
+	else:
+		if state != states.knockback or is_on_floor():
+			var damping = -sign(velocity.x) * VELOCITY/(DAMPING_DIVIDER)*delta
+			if not is_on_floor():
+				damping /= AIR_DAMPING_MULTIPLIER
+			velocity.x += damping
+			if abs(velocity.x) < MIN_VELOCITY:
+				velocity.x = 0
 
-    if is_on_floor():
-        if enable_movement and Input.is_action_just_pressed("jump"):
-            velocity.y -= JUMP_POWER
-    velocity += Globals.GRAVITY * delta
+	if is_on_floor():
+		if enable_movement and Input.is_action_just_pressed("jump"):
+			velocity.y -= JUMP_POWER
+	velocity += Globals.GRAVITY * delta
 
 var time_val = 0.0
 
 func handle_states(delta):
-    sprite.position = Vector2(0,0)
-    sword_hitbox.position = Vector2(0,0)
-    arrow_start.position = Vector2(11,17)
-    match state:
-        states.move:
-            if abs(velocity.x) > VELOCITY_ANIMATION_THRESHOLD:
-                if velocity.x < 0:
-                    sprite.flip_h = true
-                else:
-                    sprite.flip_h = false
-            if is_on_floor():
-                if abs(velocity.x) > VELOCITY_ANIMATION_THRESHOLD:
-                    anim.play("Run")
-                else:
-                    anim.play("Stand")
-            else:
-                if velocity.y < 0:
-                    anim.play("JumpUp")
-                else:
-                    anim.play("JumpDown")
-            if Input.is_action_just_pressed("sword"):
-                state = states.sword_attack
-                enable_movement = false
-                time_val = 0.0
-                if Globals.state == Globals.states.boss_ground:
-                    anim.play("SwordGround")
-                else:
-                    anim.play("SwordFall")
-            elif Input.is_action_just_pressed("bow"):
-                if arrows > 0.0:
-                    state = states.bow_prepare
-                    enable_movement = false
-                    time_val = 0.0 
-                    anim.play("Bow")
-        states.knockback:
-            time_val += delta
-            if time_val > KNOCKBACK_TIME:
-                enable_movement = true
-                state = states.move
-        states.sword_attack:
-            time_val += delta
-            if sprite.flip_h:
-                sprite.position = Vector2(-18,0)
-                sword_hitbox.position = Vector2(-16,0)
-            if time_val > SWORD_ATTACK_TIME:
-                if Globals.state == Globals.states.boss_ground:
-                    anim.play("Stand")
-                else:
-                    anim.play("FallDown")
-                state = states.move
-                enable_movement = true
-        states.bow_prepare:
-            time_val += delta
-            if sprite.flip_h:
-                sprite.position = Vector2(-18,0)
-                arrow_start.position = Vector2(27,17)
-            if time_val > BOW_PREPARE_TIME:
-                anim.play("BowFinish")
-                state = states.bow_attack
-                shoot_arrow()
-                time_val = 0.0
-        states.bow_attack:
-            time_val += delta
-            if sprite.flip_h:
-                sprite.position = Vector2(-18,0)
-            if time_val > BOW_ATTACK_TIME:
-                if Globals.state == Globals.states.boss_ground:
-                    anim.play("Stand")
-                else:
-                    anim.play("FallDown")
-                time_val = 0.0
-                state = states.move
-                enable_movement = true
+	sprite.position = Vector2(0,0)
+	sword_hitbox.position = Vector2(0,0)
+	arrow_start.position = Vector2(11,17)
+	match state:
+		states.move:
+			if abs(velocity.x) > VELOCITY_ANIMATION_THRESHOLD:
+				if velocity.x < 0:
+					sprite.flip_h = true
+				else:
+					sprite.flip_h = false
+			if is_on_floor():
+				if abs(velocity.x) > VELOCITY_ANIMATION_THRESHOLD:
+					anim.play("Run")
+				else:
+					anim.play("Stand")
+			else:
+				if velocity.y < 0:
+					anim.play("JumpUp")
+				else:
+					anim.play("JumpDown")
+			if Input.is_action_just_pressed("sword"):
+				state = states.sword_attack
+				enable_movement = false
+				time_val = 0.0
+				if Globals.state == Globals.states.boss_ground:
+					anim.play("SwordGround")
+				else:
+					anim.play("SwordFall")
+			elif Input.is_action_just_pressed("bow"):
+				if arrows > 0.0:
+					state = states.bow_prepare
+					enable_movement = false
+					time_val = 0.0 
+					anim.play("Bow")
+		states.knockback:
+			time_val += delta
+			if time_val > KNOCKBACK_TIME:
+				enable_movement = true
+				state = states.move
+		states.sword_attack:
+			time_val += delta
+			if sprite.flip_h:
+				sprite.position = Vector2(-18,0)
+				sword_hitbox.position = Vector2(-16,0)
+			if time_val > SWORD_ATTACK_TIME:
+				if Globals.state == Globals.states.boss_ground:
+					anim.play("Stand")
+				else:
+					anim.play("FallDown")
+				state = states.move
+				enable_movement = true
+		states.bow_prepare:
+			time_val += delta
+			if sprite.flip_h:
+				sprite.position = Vector2(-18,0)
+				arrow_start.position = Vector2(27,17)
+			if time_val > BOW_PREPARE_TIME:
+				anim.play("BowFinish")
+				state = states.bow_attack
+				shoot_arrow()
+				time_val = 0.0
+		states.bow_attack:
+			time_val += delta
+			if sprite.flip_h:
+				sprite.position = Vector2(-18,0)
+			if time_val > BOW_ATTACK_TIME:
+				if Globals.state == Globals.states.boss_ground:
+					anim.play("Stand")
+				else:
+					anim.play("FallDown")
+				time_val = 0.0
+				state = states.move
+				enable_movement = true
 
 func _physics_process(delta):
-    ground_movement(delta)
-    velocity = move_and_slide(velocity,Vector2(0,-1))
+	ground_movement(delta)
+	velocity = move_and_slide(velocity,Vector2(0,-1))
 
 func _process(delta):
-    handle_states(delta)
-    
+	handle_states(delta)
+	
 func damage(amount):
-    health = clamp(health-amount,0.0,health_bar.MAX_VALUE)
-    health_bar.set_value(health)
-    
+	health = clamp(health-amount,0.0,health_bar.MAX_VALUE)
+	health_bar.set_value(health)
+	
 func knockback(pos,multiplier=1.0):
-    if state != states.knockback:
-        if Globals.state == Globals.states.boss_ground:
-            anim.play("KnockbackGround")
-        else:
-            anim.play("KnockbackFall")
-        velocity = (position - pos).normalized() * KNOCKBACK_POWER * multiplier
-        enable_movement = false
-        sword_hitbox.monitoring = false
-        time_val = 0.0
-    state = states.knockback
+	if state != states.knockback:
+		if Globals.state == Globals.states.boss_ground:
+			anim.play("KnockbackGround")
+		else:
+			anim.play("KnockbackFall")
+		velocity = (position - pos).normalized() * KNOCKBACK_POWER * multiplier
+		enable_movement = false
+		sword_hitbox.monitoring = false
+		time_val = 0.0
+	state = states.knockback
 
 func on_hitbox_entered(body):
-    if body.can_inflict_damage() and can_be_damaged():
-        knockback(body.position + Vector2(0,60))
-        damage(ODIN_TOUCH_DAMAGE)
+	if body.can_inflict_damage() and can_be_damaged():
+		knockback(body.position + Vector2(0,60))
+		damage(ODIN_TOUCH_DAMAGE)
 
 func on_sword_hitbox_entered(body):
-    if body.can_be_damaged():
-        body.knockback()
-        body.damage(SWORD_DAMAGE)
+	if body.can_be_damaged():
+		body.knockback()
+		body.damage(SWORD_DAMAGE)
 
 func can_be_damaged():
-    match state:
-        states.knockback: return false
-    return true
+	match state:
+		states.knockback: return false
+	return true
 
 func shoot_arrow():
-    arrows = max(arrows-2.0,0.0)
-    arrow_bar.set_value(arrows)
+	arrows = max(arrows-2.0,0.0)
+	arrow_bar.set_value(arrows)
 
-    var arrow = arrow_scene.instance()
-    arrow.position = arrow_start.global_position
-    if sprite.flip_h:
-        arrow.position += Vector2(-arrow.texture.get_width(),0)
-    else:
-        arrow.flip_h = true
-    get_tree().get_root().add_child(arrow)
+	var arrow = arrow_scene.instance()
+	arrow.position = arrow_start.global_position
+	if sprite.flip_h:
+		arrow.position += Vector2(-arrow.texture.get_width(),0)
+	else:
+		arrow.flip_h = true
+	get_tree().get_root().add_child(arrow)
 
 func collect_arrow(amount):
-    arrows = min(arrows+amount,arrow_bar.MAX_VALUE)
-    arrow_bar.set_value(arrows)
+	arrows = min(arrows+amount,arrow_bar.MAX_VALUE)
+	arrow_bar.set_value(arrows)
 
 func get_position():
-    return position + Vector2(width,height)/2.0
+	return position + Vector2(width,height)/2.0

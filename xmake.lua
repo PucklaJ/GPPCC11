737c7f141c308620064ef6e503b8f3881902b3af
PucklaJ <jonaas.pucher000000@gmail.com>
2022-11-30 00:16:28 +0100

    Remove add_repositories in xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,5 @@
 set_project("GPPCC11")
 
-add_repositories("PucklaMotzer09::JohnnyEngine https://github.com/PucklaMotzer09/xmake-repo.git johnnyengine")
-add_repositories("PucklaMotzer09::cAudio https://github.com/PucklaMotzer09/xmake-repo.git caudio")
 add_requires("johnnyengine", "caudio")
 
 add_rules("mode.debug", "mode.release")
